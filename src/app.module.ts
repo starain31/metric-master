8894d672125a218eddfe5c09ebefce140096e5a8
Nazmus Sakib <nsakib.cse@gmail.com>
2023-12-04 10:48:57 +0600

    Add LogsService to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogsService } from './logs/logs.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LogsService],
 })
 export class AppModule {}
