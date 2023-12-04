d73d8b7b4d544a2068db440f81322834df5066d1
Nazmus Sakib <nsakib.cse@gmail.com>
2023-12-04 11:12:08 +0600

    Add DataSourceService to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsService } from './logs/logs.service';
+import { DataSourceService } from './data-source/data-source.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, LogsService],
+  providers: [AppService, LogsService, DataSourceService],
 })
 export class AppModule {}
