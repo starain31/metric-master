6daea0e2dbce32fddaac4b63285a44ba8c5ff937
Nazmus Sakib <nsakib.cse@gmail.com>
2023-12-04 10:50:12 +0600

    Remove logs from .gitignore

---
+++ src/logs/logs.service.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LogsService } from './logs.service';
+
+describe('LogsService', () => {
+  let service: LogsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LogsService],
+    }).compile();
+
+    service = module.get<LogsService>(LogsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of logs', () => {
+    const result = service.findAll();
+    expect(result).toBeInstanceOf(Array);
+  });
+});
---
+++ src/logs/logs.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LogsService {
+  findAll() {
+    throw new Error('Method not implemented.');
+  }
+}
